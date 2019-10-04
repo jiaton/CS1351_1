9ff87c3c5ae03f99d2b8905fb250b65e17ae58b7
Tong Jia <792911138@qq.com>
2019-10-04 11:12:22 -0700

    initiallized with part 1.1 and half of 1.2

---
+++ Assets/Lightswitch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightswitch : MonoBehaviour
+{
+    public Light light;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("tab"))
+        {
+            if (light.color.ToString().Equals((new Color(0, 255, 0).ToString())))
+                light.color = new Color(255, 0, 0);   
+            else
+            {
+                light.color = new Color(0, 255, 0);
+
+            }
+        }
+    }
+}
